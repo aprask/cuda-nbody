7bb36b629906dedb00689b321333e460493982de
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2025-05-05 16:08:35 -0400

    added

---
+++ cpu_run2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=nbod1
+#SBATCH --partition=GPU
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=01:00:00
+module load cuda
+nvidia-smi
+nvcc -Xcompiler -fopenmp nbody.cu -lpthread -o nbody
+./nbody 10000 10 5 1 cpu
